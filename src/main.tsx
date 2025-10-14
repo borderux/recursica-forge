00d1a6ecd761e03ebb23624cfd4c1995c50cf2ea
Aaron Martlage <aaron@borderux.com>
2025-10-14 16:10:16 -0700

    feat(theme): add theme module and integrate in main.tsx; update package files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { App } from './modules/app/App'
+import { CodePenPage } from './modules/theme/index'
 import './styles/index.css'
 import './styles/theme.css.ts'
 
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/theme', element: <CodePenPage /> },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
