f12e901e278dca4a949ad87ee3b3cb30dd4ed4a1
Sandra Ornelas <sandra@borderux.com>
2025-10-29 22:38:03 -0600

    chore: update font line height tokens and build config

--- src/modules/tokens/FontLineHeightTokens.tsx
+++ src/modules/tokens/FontLineHeightTokens.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useVars } from '../vars/VarsContext'
 import { readOverrides, setOverride } from '../theme/tokenOverrides'
 
@@ -118,8 +118,8 @@ export default function FontLineHeightTokens() {
           const isTall = k === 'tall'
           const disabled = scaleByST && !(isDefault || isShort || isTall)
           return (
-            <>
-              <label key={name + '-label'} htmlFor={name} style={{ fontSize: 13, opacity: 0.9 }}>{label}</label>
+            <React.Fragment key={name}>
+              <label htmlFor={name} style={{ fontSize: 13, opacity: 0.9 }}>{label}</label>
               <input
                 type="range"
                 min={0.5}
@@ -162,7 +162,7 @@ export default function FontLineHeightTokens() {
                 }}
                 style={{ width: 80, paddingRight: 0, textAlign: 'right' }}
               />
-            </>
+            </React.Fragment>
           )
         })}
       </div>
